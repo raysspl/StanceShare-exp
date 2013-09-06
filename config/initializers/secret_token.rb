fa9850fdeea6e933f98b55040bd7011b74ed83ac
ray ray <raydipasupil@gmail.com>
2013-09-06 10:54:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stanceshare2::Application.config.secret_key_base = 'dbb073b71f2b4f78e91438810784f26bfcd55eedf453e6c1b054b4bea8473b9adf6148ce9e2146d9116d20c37be4e444fffcfb4fe1ac81ceeeadfd50e46209ee'
